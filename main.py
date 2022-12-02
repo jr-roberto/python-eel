0397713f4bffe0c3da0e45f03143c0d87f5e8fe9
jr-roberto <robertoti.adm@gmail.com>
2022-12-02 14:14:07 -0300

    testes

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import eel
+import sqlite3
+
+con = sqlite3.connect("sistema_python.db")
+cur = con.cursor()
+res = cur.execute("SELECT name FROM sqlite_master")
+
+print(res.fetchone())
 
 eel.init("web")
 
 @eel.expose
-def nova_tabela():
+def login():
     return eel.BOTTLE_ROUTES
 
 eel.start("index.html")
