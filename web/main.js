3db3f73c31e1009652d8a01c594095598ea4a372
jr-roberto <robertoti.adm@gmail.com>
2022-12-02 13:44:46 -0300

    Ajustado

--- web/main.js
+++ web/main.js
@@ -1,15 +1,6 @@
+// enviando comando para python e recebendo resposta
 const button = document.getElementById("btn_teste").onclick = function(){
-    var campo = document.getElementById("campo").value;
     eel.nova_tabela()((output)=>{
         window.alert(output)
     });
 }
-
-function finalizar(){
-    close();
-    /* window.open("https://web.whatsapp.com/") */
-}
-
-setTimeout(()=>{
-    window.alert(`width : ${screen.width} || height : ${screen.height}`)
-},5000);
